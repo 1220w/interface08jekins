108a2648e7e64e3ac07edef6e0e81736be836240
Name <email@example.com>
2019-10-17 16:26:22 +0800

    v2.0

--- run_suite.py
+++ run_suite.py
@@ -2,6 +2,7 @@ import unittest
 
 from case.TestIHRMEmploye import TestIHRMEmp
 from case.TestIHRMUser import TestIHRMUser
+from tools.HTMLTestRunner import HTMLTestRunner
 
 suite = unittest.TestSuite()
 
@@ -10,5 +11,9 @@ suite.addTest(TestIHRMEmp("test_emp_add"))
 suite.addTest(TestIHRMEmp("test_emp_uppdate"))
 suite.addTest(TestIHRMEmp("test_emp_delete"))
 #
-runner = unittest.TextTestRunner()
+file_to = "./report/report.html"
+with open(file_to, "wb")as f:
+    runner = HTMLTestRunner(f, title ="我的测试报告",description="v1.0")
+    # runner = unittest.TextTestRunner()
+
 runner.run(suite)
